cfa71d6b892b3b4b68daa34ad1d8a230eadcc486
Bence Tóth <bence.zltn.toth@gmail.com>
2022-12-13 15:33:13 +0100

    added directors

--- streamlit.py
+++ streamlit.py
@@ -9,7 +9,7 @@ from sklearn import metrics
 from joblib import load
 import streamlit as st
 
-loaded_model = load_model('nn1.h5')
+loaded_model = load_model('nn1_d.h5')
 
 def office_prediction(input_data):
     ss = load('scaler1.bin')
@@ -23,15 +23,32 @@ def office_prediction(input_data):
 if __name__ == '__main__':
     st.title('Office Ratings Prediction')
 
+    Director_Options = ['Ken Kwapis', 'Ken Whittingham', 'Bryan Gordon', 'Greg Daniels',
+       'Amy Heckerling', 'Paul Feig', 'Charles McDougall',
+       'Dennie Gordon', 'Victor Nelli Jr.', 'See full summary',
+       'Roger Nygard', 'Randall Einhorn', 'Miguel Arteta', 'Tucker Gates',
+       'Jeffrey Blitz', 'Harold Ramis', 'Julian Farino', 'Joss Whedon',
+       'J.J. Abrams', 'Craig Zisk', 'Paul Lieberstein', 'Jason Reitman',
+       'Jennifer Celotta', 'David Rogers', 'Stephen Merchant',
+       'Dean Holland', 'Asaad Kelada', 'Gene Stupnitsky', 'Steve Carell',
+       'Brent Forrester', 'Lee Eisenberg', 'Reginald Hudlin',
+       'Seth Gordon', 'B.J. Novak', 'John Krasinski', 'Marc Webb',
+       'Matt Sohn', 'Mindy Kaling', 'Rainn Wilson', 'John Scott',
+       'Alex Hardcastle', 'Troy Miller', 'Charlie Grandy', 'Ed Helms',
+       'Eric Appel', 'Brian Baumgartner', 'Claire Scanlon', 'Daniel Chun',
+       'Bryan Cranston', 'Rodman Flender', 'Kelly Cantley-Kashima',
+       'Lee Kirk', 'Jon Favreau', 'Jesse Peretz']
+
     Season = st.slider('Season', min_value=1, max_value=9)
     Votes = st.number_input('Number of votes', step=100, value=0)
     Viewership = st.number_input('Viewership of the episode', step=0.1, value=0.0)
     Duration = st.number_input('Duration of the episode', step=1, value=0)
+    Director = st.selectbox("Director of the episode", options=Director_Options)
 
     #input_data = np.array([[Season, Votes, Viewership, Duration]])
      #prediction = office_prediction(input_data)
     
-    prediction = office_prediction([[Season, Votes, Viewership, Duration]])
+    prediction = office_prediction([[Season, Votes, Viewership, Duration, Director]])
     # prediction = round(prediction, 2)
 
     if st.button('Predict Episode Rating'):
