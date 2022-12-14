b72c6fb482940579e75e7864f0b4406d778c702d
Bence Tóth <bence.zltn.toth@gmail.com>
2022-12-14 12:05:18 +0100

    viewership default value

--- streamlit.py
+++ streamlit.py
@@ -52,7 +52,7 @@ if __name__ == '__main__':
 
     Season = st.slider('Season', min_value=1, max_value=9)
     Votes = st.number_input('Number of votes', step=100, value=0)
-    Viewership = st.number_input('Viewership of the episode', step=0.1, value=0.0)
+    Viewership = st.number_input('Viewership of the episode', step=0.1, value=0)
     Duration = st.number_input('Duration of the episode', step=1, value=0)
     Director = st.selectbox("Director of the episode", options=Director_Options)
 
